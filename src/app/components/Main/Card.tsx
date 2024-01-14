e74ab2eaf4c4cf26c56d40cf35cc0bb9f1ac58a1
leonardo.bombassei <leonardo.bombassei@doubleit.com.br>
2024-01-13 23:16:24 -0300

    FEAT: Adicionando novas imagens

--- src/app/components/Main/Card.tsx
+++ src/app/components/Main/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ src, alt }) => (
   <a className='bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/10 transition-colors'>
-    <Image src={src} width={104} height={104} alt={alt}/>
+    <Image src={src} width={96} height={96} alt={alt} className="h-full"/>
     <strong>{alt}</strong>
     <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-10 invisible group-hover:visible transition'>
       <Play fill='bg-black'/>
--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -9,18 +9,23 @@ export function Main() {
       <PaginationControls />
       <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
       <div className='grid grid-cols-3 gap-4 mt-4'>
-        {[1, 2, 3, 4, 5, 6].map((index) => (
-          <Card key={index} src="/powerslave.jpg" alt="PowerSlave" />
-        ))}
+        
+          <Card key={1} src="/powerslave.jpg" alt="PowerSlave" />
+          <Card key={2} src="/thisIsImagineDragons.jpg" alt="This is Imagine Dragons" />
+          <Card key={3} src="/thisIsIronMaiden.jpg" alt="This is Iron Maiden" />
+          <Card key={4} src="/Gospel.png" alt="Gospel" />
+          <Card key={5} src="/haikaiss.png" alt="Haikaiss e Costa Gold" />
+          <Card key={6} src="/iconic.png" alt="Iconic Soundtracks" />
+        
       </div>
       <SectionWithTitle
         title="Made for Leonardo Bombassei"
         items={[
-          { src: "/powerslave.jpg", alt: "Daily Mix 1", description: "Wallows, COIN, girl in red and more" },
-          { src: "/powerslave.jpg", alt: "Daily Mix 2", description: "Some other artists" },
-          { src: "/powerslave.jpg", alt: "Daily Mix 3", description: "Some other artists" },
-          { src: "/powerslave.jpg", alt: "Daily Mix 4", description: "Some other artists" },
-          { src: "/powerslave.jpg", alt: "Daily Mix 5", description: "Some other artists" },
+          { src: "/dm1.jpeg", alt: "Daily Mix 1", description: "The Worship Initiative, Hillsong Worship,..." },
+          { src: "/dm2.jpeg", alt: "Daily Mix 2", description: "Some other artists" },
+          { src: "/dm3.jpeg", alt: "Daily Mix 3", description: "Some other artists" },
+          { src: "/dm4.jpeg", alt: "Daily Mix 4", description: "Some other artists" },
+          { src: "/dm5.jpeg", alt: "Daily Mix 5", description: "Some other artists" },
         ]}
       />
     </main>
